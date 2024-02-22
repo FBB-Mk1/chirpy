71122d2cf319e559c4ea0dccf05948ac524a7671
fabin <fabiobatista@gmail.com>
2024-02-21 21:36:17 -0300

    setting up server basics

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: corsMux,
+	}
+	err := srv.ListenAndServe()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
