5782c91af46a61344948c47490faa75dd5d3e968
fabin <fabiobatista@gmail.com>
2024-02-26 19:41:24 -0300

    a

--- metrics.go
+++ metrics.go
@@ -13,9 +13,9 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits)
+	msg := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits)
 	w.Write([]byte(msg))
 }
 
