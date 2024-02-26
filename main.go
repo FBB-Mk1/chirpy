3208be6ffff2716f3bf603517c178ce0e283ea8f
fabin <fabiobatista@gmail.com>
2024-02-25 22:21:55 -0300

    CH 3.1 - Monolits and Decoupling

--- main.go
+++ main.go
@@ -12,24 +12,29 @@ type apiConfig struct {
 }
 
 func main() {
+	port := "8080"
 	apiCfg := apiConfig{}
 	rt := chi.NewRouter()
+	apiRt := chi.NewRouter()
 	corsMux := middlewareCors(rt)
+
 	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
-	rt.Handle("/app", fsHandler)
-	rt.Handle("/app/*", fsHandler)
-	rt.Get("/healthz", healthzHandler)
-	rt.Get("/metrics", apiCfg.metricsHandler)
-	rt.HandleFunc("/reset", apiCfg.resetHandler)
+	rt.Mount("/api", apiRt)
+	rt.Mount("/app", fsHandler)
+
+	apiRt.Get("/healthz", healthzHandler)
+	apiRt.Get("/metrics", apiCfg.metricsHandler)
+	apiRt.HandleFunc("/reset", apiCfg.resetHandler)
 
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: corsMux,
 	}
-	err := srv.ListenAndServe()
 
+	err := srv.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
+
 }
